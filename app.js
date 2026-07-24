79ebfc82e60ddd57852d9d704ae5fe9ff418b6e6
Dagm Tesfu <lenovotablety847@gmail.com>
2026-07-24 17:12:15 +0300

    Inital Commit

---
+++ app.js
@@ -0,0 +1,83 @@
+const workouts = [
+    {
+    name: "Push-ups",
+    target: "Chest and Triceps",
+    goal: "Strength and Muscle",
+    difficulty: "Begineer and Advacned",
+    sets: 3,
+    reps: 15
+},
+{
+    name: "Bench Press",
+    target: "Chest and Triceps",
+    goal: "Strength",
+    difficulty: "Begineer and Advanced",
+    sets: 3,
+    reps: 10
+},
+{
+    name: "Incline Bench Press",
+    target: "Chest",
+    goal: "Strength and Muscle",
+    difficulty: "Begineer and Advanced",
+    sets: 3,
+    reps: 10
+},
+{
+    name: "Dips",
+    target: "Chest and Triceps",
+    goal: "Strength and Muscle",
+    difficulty: "Begineer and Advanced",
+    sets: 3,
+    reps: 15
+},
+{
+    name: "Cable Fly",
+    target: "Chest",
+    goal: "Muscle",
+    difficulty: "Begineer and Advanced",
+    sets: 3,
+    reps: 12
+},
+{
+    name: "Lat Pulldown",
+    target: "Back(Lat Muscle)",
+    goal: "Muscle",
+    difficulty: "Begineer and Advanced",
+    sets: 3,
+    reps: 15
+},
+{
+    name: "Pull-Up",
+    target: "Back(Lat)",
+    goal: "Muscle",
+    difficulty: "Begineer and Advanced",
+    sets: 3,
+    reps: 10
+},
+{
+    name: "Chest Suported Row",
+    target: "Back",
+    goal: "Muscle",
+    difficulty: "Begineer and Advanced",
+    sets: 3,
+    reps: 15
+},
+{
+    name: "Push-ups",
+    target: "Ches and Triceps",
+    goal: "Strength and Muscle",
+    difficulty: "Begineer",
+    sets: 3,
+    reps: 15
+},
+{
+    name: "Push-ups",
+    target: "Ches and Triceps",
+    goal: "Strength and Muscle",
+    difficulty: "Begineer",
+    sets: 3,
+    reps: 15
+},
+
+]
