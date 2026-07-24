67f093c2ebe9575c466cc47fa7d4339417ce49df
Dagm Tesfu <lenovotablety847@gmail.com>
2026-07-24 17:57:30 +0300

    Updated the workouts

--- app.js
+++ app.js
@@ -3,23 +3,23 @@ const workouts = [
     name: "Push-ups",
     target: "Chest and Triceps",
     goal: ["muscle", "Athletic", "Strength"],
-    difficulty: ["Begineer", "Intermidiate"],
+    difficulty: ["Beginner"],
     sets: 3,
-    reps: 15
+    reps: 10
 },
 {
     name: "Bench Press",
     target: "Chest and Triceps",
     goal: "Strength",
-    difficulty: "Begineer and Advanced",
+    difficulty: ["Intermediate"],
     sets: 3,
-    reps: 10
+    reps: 8
 },
 {
     name: "Incline Bench Press",
     target: "Chest",
     goal: "Strength and Muscle",
-    difficulty: "Begineer and Advanced",
+    difficulty: ["Intermediate"],
     sets: 3,
     reps: 10
 },
@@ -27,15 +27,15 @@ const workouts = [
     name: "Dips",
     target: "Chest and Triceps",
     goal: "Strength and Muscle",
-    difficulty: "Begineer and Advanced",
+    difficulty: ["Intermediate"],
     sets: 3,
-    reps: 15
+    reps: 8
 },
 {
     name: "Cable Fly",
     target: "Chest",
     goal: "Muscle",
-    difficulty: "Begineer and Advanced",
+    difficulty: ["Beginner"],
     sets: 3,
     reps: 12
 },
@@ -43,152 +43,152 @@ const workouts = [
     name: "Lat Pulldown",
     target: "Back(Lat Muscle)",
     goal: "Muscle",
-    difficulty: "Begineer and Advanced",
+    difficulty: ["Beginner"],
     sets: 3,
-    reps: 15
+    reps: 12
 },
 {
     name: "Pull-Up",
     target: "Back(Lat)",
     goal: "Muscle",
-    difficulty: "Begineer and Advanced",
+    difficulty: ["Intermediate"],
     sets: 3,
-    reps: 10
+    reps: 6
 },
 {
     name: "Chest Suported Row",
     target: "Back",
     goal: "Muscle",
-    difficulty: "Begineer and Advanced",
+    difficulty: ["Beginner"],
     sets: 3,
-    reps: 15
+    reps: 12
 },
 {
     name: "Push-ups",
     target: "Ches and Triceps",
     goal: "Strength and Muscle",
-    difficulty: "Begineer",
+    difficulty: ["Beginner"],
     sets: 3,
-    reps: 15
+    reps: 10
 },
 {
     name: "Push-ups",
     target: "Ches and Triceps",
     goal: "Strength and Muscle",
-    difficulty: "Begineer",
+    difficulty: ["Beginner"],
     sets: 3,
-    reps: 15
+    reps: 10
 },
 
 {
     name: "Lat Pulldown",
     target: "Back",
     goal: ["muscle", "Athletic"],
-    difficulty: ["Begineer", "Intermidiate"],
+    difficulty: ["Beginner"],
     sets: 3,
-    reps: 15
+    reps: 12
 },
 {
     name: "Seated Cable Row",
     target: "Back",
     goal: ["muscle", "Athletic"],
-    difficulty: ["Begineer", "Intermidiate"],
+    difficulty: ["Beginner"],
     sets: 3,
-    reps: 15
+    reps: 12
 },
 {
     name: "Barbell Bent-Over Row",
     target: "Back",
     goal: ["muscle", "Athletic", "Strength"],
-    difficulty: ["Begineer", "Intermidiate"],
+    difficulty: ["Intermediate"],
     sets: 3,
-    reps: 15
+    reps: 8
 },
 {
     name: "One-Arm Dumbbell Row",
     target: "Back",
     goal: ["muscle", "Athletic"],
-    difficulty: ["Begineer", "Intermidiate"],
+    difficulty: ["Intermediate"],
     sets: 3,
-    reps: 15
+    reps: 10
 },
 {
     name: "Chest-Supported Dumbbell Row",
     target: "Back",
     goal: ["muscle"],
-    difficulty: ["Begineer", "Intermidiate"],
+    difficulty: ["Beginner"],
     sets: 3,
-    reps: 15
+    reps: 12
 },
 {
     name: "Assisted Pull-Up",
     target: "Back",
     goal: ["muscle", "Athletic"],
-    difficulty: ["Begineer", "Intermidiate"],
+    difficulty: ["Beginner"],
     sets: 3,
-    reps: 15
+    reps: 10
 },
 {
     name: "T-Bar Row",
     target: "Back",
     goal: ["muscle", "Athletic", "Strength"],
-    difficulty: ["Begineer", "Intermidiate"],
+    difficulty: ["Intermediate"],
     sets: 3,
-    reps: 15
+    reps: 8
 },
 {
     name: "Straight-Arm Cable Pulldown",
     target: "Back",
     goal: ["muscle"],
-    difficulty: ["Begineer", "Intermidiate"],
+    difficulty: ["Beginner"],
     sets: 3,
-    reps: 15
+    reps: 12
 },
 {
     name: "Barbell Bench Press",
     target: "Chest",
     goal: ["muscle", "Athletic", "Strength"],
-    difficulty: ["Begineer", "Intermidiate"],
+    difficulty: ["Intermediate"],
     sets: 3,
-    reps: 15
+    reps: 8
 },
 {
     name: "Dumbbell Bench Press",
     target: "Chest",
     goal: ["muscle", "Strength"],
-    difficulty: ["Begineer", "Intermidiate"],
+    difficulty: ["Intermediate"],
     sets: 3,
-    reps: 15
+    reps: 10
 },
 {
     name: "Incline Barbell Bench Press",
     target: "Chest",
     goal: ["muscle", "Athletic", "Strength"],
-    difficulty: ["Begineer", "Intermidiate"],
+    difficulty: ["Intermediate"],
     sets: 3,
-    reps: 15
+    reps: 8
 },
 {
     name: "Incline Dumbbell Press",
     target: "Chest",
     goal: ["muscle", "Strength"],
-    difficulty: ["Begineer", "Intermidiate"],
+    difficulty: ["Intermediate"],
     sets: 3,
-    reps: 15
+    reps: 10
 },
 {
     name: "Chest Press Machine",
     target: "Chest",
     goal: ["muscle"],
-    difficulty: ["Begineer", "Intermidiate"],
+    difficulty: ["Beginner"],
     sets: 3,
-    reps: 15
+    reps: 12
 },
 {
     name: "Pec Deck Fly",
     target: "Chest",
     goal: ["muscle"],
-    difficulty: ["Begineer", "Intermidiate"],
+    difficulty: ["Beginner"],
     sets: 3,
     reps: 15
 },
@@ -196,7 +196,7 @@ const workouts = [
     name: "Cable Chest Fly",
     target: "Chest",
     goal: ["muscle"],
-    difficulty: ["Begineer", "Intermidiate"],
+    difficulty: ["Beginner"],
     sets: 3,
     reps: 15
 },
@@ -204,39 +204,39 @@ const workouts = [
     name: "Dumbbell Chest Fly",
     target: "Chest",
     goal: ["muscle"],
-    difficulty: ["Begineer", "Intermidiate"],
+    difficulty: ["Intermediate"],
     sets: 3,
-    reps: 15
+    reps: 12
 },
 {
     name: "Dumbbell Shoulder Press",
     target: "Shoulders",
     goal: ["muscle", "Strength"],
-    difficulty: ["Begineer", "Intermidiate"],
+    difficulty: ["Intermediate"],
     sets: 3,
-    reps: 15
+    reps: 10
 },
 {
     name: "Barbell Overhead Press",
     target: "Shoulders",
     goal: ["muscle", "Athletic", "Strength"],
-    difficulty: ["Begineer", "Intermidiate"],
+    difficulty: ["Intermediate"],
     sets: 3,
-    reps: 15
+    reps: 8
 },
 {
     name: "Shoulder Press Machine",
     target: "Shoulders",
     goal: ["muscle"],
-    difficulty: ["Begineer", "Intermidiate"],
+    difficulty: ["Beginner"],
     sets: 3,
-    reps: 15
+    reps: 12
 },
 {
     name: "Dumbbell Lateral Raise",
     target: "Shoulders",
     goal: ["muscle"],
-    difficulty: ["Begineer", "Intermidiate"],
+    difficulty: ["Beginner"],
     sets: 3,
     reps: 15
 },
@@ -244,7 +244,7 @@ const workouts = [
     name: "Cable Lateral Raise",
     target: "Shoulders",
     goal: ["muscle"],
-    difficulty: ["Begineer", "Intermidiate"],
+    difficulty: ["Beginner"],
     sets: 3,
     reps: 15
 },
@@ -252,7 +252,7 @@ const workouts = [
     name: "Rear Delt Fly Machine",
     target: "Shoulders",
     goal: ["muscle"],
-    difficulty: ["Begineer", "Intermidiate"],
+    difficulty: ["Beginner"],
     sets: 3,
     reps: 15
 },
@@ -260,7 +260,7 @@ const workouts = [
     name: "Dumbbell Rear Delt Fly",
     target: "Shoulders",
     goal: ["muscle"],
-    difficulty: ["Begineer", "Intermidiate"],
+    difficulty: ["Intermediate"],
     sets: 3,
     reps: 15
 },
@@ -268,7 +268,7 @@ const workouts = [
     name: "Cable Face Pull",
     target: "Shoulders",
     goal: ["muscle", "Athletic"],
-    difficulty: ["Begineer", "Intermidiate"],
+    difficulty: ["Beginner"],
     sets: 3,
     reps: 15
 },
@@ -276,287 +276,287 @@ const workouts = [
     name: "Barbell Curl",
     target: "Biceps",
     goal: ["muscle", "Strength"],
-    difficulty: ["Begineer", "Intermidiate"],
+    difficulty: ["Intermediate"],
     sets: 3,
-    reps: 15
+    reps: 10
 },
 {
     name: "Dumbbell Curl",
     target: "Biceps",
     goal: ["muscle"],
-    difficulty: ["Begineer", "Intermidiate"],
+    difficulty: ["Beginner"],
     sets: 3,
-    reps: 15
+    reps: 12
 },
 {
     name: "Hammer Curl",
     target: "Biceps",
     goal: ["muscle", "Strength"],
-    difficulty: ["Begineer", "Intermidiate"],
+    difficulty: ["Beginner"],
     sets: 3,
-    reps: 15
+    reps: 12
 },
 {
     name: "EZ-Bar Curl",
     target: "Biceps",
     goal: ["muscle", "Strength"],
-    difficulty: ["Begineer", "Intermidiate"],
+    difficulty: ["Intermediate"],
     sets: 3,
-    reps: 15
+    reps: 10
 },
 {
     name: "Preacher Curl Machine",
     target: "Biceps",
     goal: ["muscle"],
-    difficulty: ["Begineer", "Intermidiate"],
+    difficulty: ["Beginner"],
     sets: 3,
-    reps: 15
+    reps: 12
 },
 {
     name: "Cable Curl",
     target: "Biceps",
     goal: ["muscle"],
-    difficulty: ["Begineer", "Intermidiate"],
+    difficulty: ["Beginner"],
     sets: 3,
-    reps: 15
+    reps: 12
 },
 {
     name: "Incline Dumbbell Curl",
     target: "Biceps",
     goal: ["muscle"],
-    difficulty: ["Begineer", "Intermidiate"],
+    difficulty: ["Intermediate"],
     sets: 3,
-    reps: 15
+    reps: 12
 },
 {
     name: "Concentration Curl",
     target: "Biceps",
     goal: ["muscle"],
-    difficulty: ["Begineer", "Intermidiate"],
+    difficulty: ["Beginner"],
     sets: 3,
-    reps: 15
+    reps: 12
 },
 {
     name: "Cable Triceps Pushdown",
     target: "Triceps",
     goal: ["muscle"],
-    difficulty: ["Begineer", "Intermidiate"],
+    difficulty: ["Beginner"],
     sets: 3,
-    reps: 15
+    reps: 12
 },
 {
     name: "Rope Triceps Pushdown",
     target: "Triceps",
     goal: ["muscle"],
-    difficulty: ["Begineer", "Intermidiate"],
+    difficulty: ["Beginner"],
     sets: 3,
-    reps: 15
+    reps: 12
 },
 {
     name: "Overhead Cable Triceps Extension",
     target: "Triceps",
     goal: ["muscle"],
-    difficulty: ["Begineer", "Intermidiate"],
+    difficulty: ["Beginner"],
     sets: 3,
-    reps: 15
+    reps: 12
 },
 {
     name: "Dumbbell Overhead Triceps Extension",
     target: "Triceps",
     goal: ["muscle"],
-    difficulty: ["Begineer", "Intermidiate"],
+    difficulty: ["Intermediate"],
     sets: 3,
-    reps: 15
+    reps: 12
 },
 {
     name: "Skull Crushers",
     target: "Triceps",
     goal: ["muscle", "Strength"],
-    difficulty: ["Begineer", "Intermidiate"],
+    difficulty: ["Intermediate"],
     sets: 3,
-    reps: 15
+    reps: 10
 },
 {
     name: "Close-Grip Bench Press",
     target: "Triceps",
     goal: ["muscle", "Athletic", "Strength"],
-    difficulty: ["Begineer", "Intermidiate"],
+    difficulty: ["Intermediate"],
     sets: 3,
-    reps: 15
+    reps: 8
 },
 {
     name: "Assisted Dip Machine",
     target: "Triceps",
     goal: ["muscle", "Athletic"],
-    difficulty: ["Begineer", "Intermidiate"],
+    difficulty: ["Beginner"],
     sets: 3,
-    reps: 15
+    reps: 10
 },
 {
     name: "Triceps Extension Machine",
     target: "Triceps",
     goal: ["muscle"],
-    difficulty: ["Begineer", "Intermidiate"],
+    difficulty: ["Beginner"],
     sets: 3,
-    reps: 15
+    reps: 12
 },
 {
     name: "Barbell Back Squat",
     target: "Legs / Quadriceps",
     goal: ["muscle", "Athletic", "Strength"],
-    difficulty: ["Begineer", "Intermidiate"],
+    difficulty: ["Intermediate"],
     sets: 3,
-    reps: 15
+    reps: 8
 },
 {
     name: "Front Squat",
     target: "Legs / Quadriceps",
     goal: ["muscle", "Athletic", "Strength"],
-    difficulty: ["Begineer", "Intermidiate"],
+    difficulty: ["Intermediate"],
     sets: 3,
-    reps: 15
+    reps: 8
 },
 {
     name: "Leg Press",
     target: "Legs / Quadriceps",
     goal: ["muscle", "Strength"],
-    difficulty: ["Begineer", "Intermidiate"],
+    difficulty: ["Beginner"],
     sets: 3,
-    reps: 15
+    reps: 10
 },
 {
     name: "Hack Squat Machine",
     target: "Legs / Quadriceps",
     goal: ["muscle", "Strength"],
-    difficulty: ["Begineer", "Intermidiate"],
+    difficulty: ["Intermediate"],
     sets: 3,
-    reps: 15
+    reps: 10
 },
 {
     name: "Leg Extension Machine",
     target: "Legs / Quadriceps",
     goal: ["muscle"],
-    difficulty: ["Begineer", "Intermidiate"],
+    difficulty: ["Beginner"],
     sets: 3,
-    reps: 15
+    reps: 12
 },
 {
     name: "Dumbbell Goblet Squat",
     target: "Legs / Quadriceps",
     goal: ["muscle", "Athletic"],
-    difficulty: ["Begineer", "Intermidiate"],
+    difficulty: ["Beginner"],
     sets: 3,
-    reps: 15
+    reps: 12
 },
 {
     name: "Bulgarian Split Squat",
     target: "Legs / Quadriceps",
     goal: ["muscle", "Athletic", "Strength"],
-    difficulty: ["Begineer", "Intermidiate"],
+    difficulty: ["Intermediate"],
     sets: 3,
-    reps: 15
+    reps: 10
 },
 {
     name: "Dumbbell Walking Lunge",
     target: "Legs / Quadriceps",
     goal: ["muscle", "Athletic"],
-    difficulty: ["Begineer", "Intermidiate"],
+    difficulty: ["Intermediate"],
     sets: 3,
-    reps: 15
+    reps: 10
 },
 {
     name: "Romanian Deadlift",
     target: "Hamstrings",
     goal: ["muscle", "Athletic", "Strength"],
-    difficulty: ["Begineer", "Intermidiate"],
+    difficulty: ["Intermediate"],
     sets: 3,
-    reps: 15
+    reps: 8
 },
 {
     name: "Barbell Stiff-Leg Deadlift",
     target: "Hamstrings",
     goal: ["muscle", "Athletic", "Strength"],
-    difficulty: ["Begineer", "Intermidiate"],
+    difficulty: ["Intermediate"],
     sets: 3,
-    reps: 15
+    reps: 8
 },
 {
     name: "Dumbbell Romanian Deadlift",
     target: "Hamstrings",
     goal: ["muscle", "Athletic", "Strength"],
-    difficulty: ["Begineer", "Intermidiate"],
+    difficulty: ["Intermediate"],
     sets: 3,
-    reps: 15
+    reps: 10
 },
 {
     name: "Lying Leg Curl Machine",
     target: "Hamstrings",
     goal: ["muscle"],
-    difficulty: ["Begineer", "Intermidiate"],
+    difficulty: ["Beginner"],
     sets: 3,
-    reps: 15
+    reps: 12
 },
 {
     name: "Seated Leg Curl Machine",
     target: "Hamstrings",
     goal: ["muscle"],
-    difficulty: ["Begineer", "Intermidiate"],
+    difficulty: ["Beginner"],
     sets: 3,
-    reps: 15
+    reps: 12
 },
 {
     name: "Good Morning",
     target: "Hamstrings",
     goal: ["muscle", "Athletic", "Strength"],
-    difficulty: ["Begineer", "Intermidiate"],
+    difficulty: ["Intermediate"],
     sets: 3,
-    reps: 15
+    reps: 8
 },
 {
     name: "Single-Leg Romanian Deadlift",
     target: "Hamstrings",
     goal: ["muscle", "Athletic"],
-    difficulty: ["Begineer", "Intermidiate"],
+    difficulty: ["Intermediate"],
     sets: 3,
-    reps: 15
+    reps: 10
 },
 {
     name: "Cable Pull-Through",
     target: "Hamstrings",
     goal: ["muscle"],
-    difficulty: ["Begineer", "Intermidiate"],
+    difficulty: ["Beginner"],
     sets: 3,
-    reps: 15
+    reps: 12
 },
 {
     name: "Barbell Hip Thrust",
     target: "Glutes",
     goal: ["muscle", "Strength"],
-    difficulty: ["Begineer", "Intermidiate"],
+    difficulty: ["Intermediate"],
     sets: 3,
-    reps: 15
+    reps: 10
 },
 {
     name: "Smith Machine Hip Thrust",
     target: "Glutes",
     goal: ["muscle", "Strength"],
-    difficulty: ["Begineer", "Intermidiate"],
+    difficulty: ["Beginner"],
     sets: 3,
-    reps: 15
+    reps: 10
 },
 {
     name: "Dumbbell Hip Thrust",
     target: "Glutes",
     goal: ["muscle"],
-    difficulty: ["Begineer", "Intermidiate"],
+    difficulty: ["Beginner"],
     sets: 3,
-    reps: 15
+    reps: 12
 },
 {
     name: "Cable Glute Kickback",
     target: "Glutes",
     goal: ["muscle"],
-    difficulty: ["Begineer", "Intermidiate"],
+    difficulty: ["Beginner"],
     sets: 3,
     reps: 15
 },
@@ -564,31 +564,31 @@ const workouts = [
     name: "Bulgarian Split Squat",
     target: "Glutes",
     goal: ["muscle", "Athletic", "Strength"],
-    difficulty: ["Begineer", "Intermidiate"],
+    difficulty: ["Intermediate"],
     sets: 3,
-    reps: 15
+    reps: 10
 },
 {
     name: "Sumo Deadlift",
     target: "Glutes",
     goal: ["muscle", "Athletic", "Strength"],
-    difficulty: ["Begineer", "Intermidiate"],
+    difficulty: ["Intermediate"],
     sets: 3,
-    reps: 15
+    reps: 8
 },
 {
     name: "Reverse Lunge",
     target: "Glutes",
     goal: ["muscle", "Athletic"],
-    difficulty: ["Begineer", "Intermidiate"],
+    difficulty: ["Intermediate"],
     sets: 3,
-    reps: 15
+    reps: 10
 },
 {
     name: "Hip Abduction Machine",
     target: "Glutes",
     goal: ["muscle"],
-    difficulty: ["Begineer", "Intermidiate"],
+    difficulty: ["Beginner"],
     sets: 3,
     reps: 15
 },
@@ -596,7 +596,7 @@ const workouts = [
     name: "Standing Calf Raise Machine",
     target: "Calves",
     goal: ["muscle", "Strength"],
-    difficulty: ["Begineer", "Intermidiate"],
+    difficulty: ["Beginner"],
     sets: 3,
     reps: 15
 },
@@ -604,7 +604,7 @@ const workouts = [
     name: "Seated Calf Raise Machine",
     target: "Calves",
     goal: ["muscle"],
-    difficulty: ["Begineer", "Intermidiate"],
+    difficulty: ["Beginner"],
     sets: 3,
     reps: 15
 },
@@ -612,7 +612,7 @@ const workouts = [
     name: "Leg Press Calf Raise",
     target: "Calves",
     goal: ["muscle", "Strength"],
-    difficulty: ["Begineer", "Intermidiate"],
+    difficulty: ["Beginner"],
     sets: 3,
     reps: 15
 },
@@ -620,7 +620,7 @@ const workouts = [
     name: "Smith Machine Calf Raise",
     target: "Calves",
     goal: ["muscle", "Strength"],
-    difficulty: ["Begineer", "Intermidiate"],
+    difficulty: ["Beginner"],
     sets: 3,
     reps: 15
 },
@@ -628,7 +628,7 @@ const workouts = [
     name: "Dumbbell Standing Calf Raise",
     target: "Calves",
     goal: ["muscle", "Athletic"],
-    difficulty: ["Begineer", "Intermidiate"],
+    difficulty: ["Beginner"],
     sets: 3,
     reps: 15
 },
@@ -636,7 +636,7 @@ const workouts = [
     name: "Single-Leg Calf Raise",
     target: "Calves",
     goal: ["muscle", "Athletic"],
-    difficulty: ["Begineer", "Intermidiate"],
+    difficulty: ["Intermediate"],
     sets: 3,
     reps: 15
 },
@@ -644,7 +644,7 @@ const workouts = [
     name: "Donkey Calf Raise Machine",
     target: "Calves",
     goal: ["muscle"],
-    difficulty: ["Begineer", "Intermidiate"],
+    difficulty: ["Beginner"],
     sets: 3,
     reps: 15
 },
@@ -652,7 +652,7 @@ const workouts = [
     name: "Seated Dumbbell Calf Raise",
     target: "Calves",
     goal: ["muscle"],
-    difficulty: ["Begineer", "Intermidiate"],
+    difficulty: ["Beginner"],
     sets: 3,
     reps: 15
 },
